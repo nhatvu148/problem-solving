c24030a056655d3bbf3f61e2c53aa8726a875341
Akiyama <nhatvu148@gmail.com>
2021-04-17 23:38:04 +0900

    vector from array

--- reverse_array.cpp
+++ reverse_array.cpp
@@ -13,16 +13,27 @@ std::vector<int> reverseArray(std::vector<int> a)
 
 int main()
 {
-    std::vector<int> sample_array;
-    sample_array.push_back(1);
-    sample_array.push_back(2);
-    sample_array.push_back(4);
-    sample_array.push_back(6);
+    int arr1[] = {2, 3, 5, 7};
+    std::vector<int> sample_array(arr1, arr1 + sizeof(arr1) / sizeof(int));
+    // sample_array.push_back(1);
+    // sample_array.push_back(2);
+    // sample_array.push_back(4);
+    // sample_array.push_back(6);
 
     auto result = reverseArray(sample_array);
-    for (auto res = result.begin(); res != result.end(); res++) {
-        std::cout << *res << " ";
+    // for (auto res = result.begin(); res != result.end(); res++)
+    // {
+    //     std::cout << *res << " ";
+    // }
+    for (auto res : result)
+    {
+        std::cout << res << " ";
     }
+
+    std::string arr[] = {"first", "sec", "third", "fourth"};
+    std::vector<std::string> vecOfStr(arr, arr + sizeof(arr) / sizeof(std::string));
+    for (std::string str : vecOfStr)
+        std::cout << str << std::endl;
 
     return 0;
 }
