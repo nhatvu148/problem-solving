55f2432d3b7bd52462e152ae3ea40045184240bd
Nhật Vũ <nhatvu148@gmail.com>
2021-09-07 12:55:29 +0900

    Create prob5.go

---
+++ Golang/prob5.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'customSorting' function below.
+ *
+ * The function is expected to return a STRING_ARRAY.
+ * The function accepts STRING_ARRAY strArr as parameter.
+ */
+
+func customSorting(strArr []string) []string {
+	return []string{""}
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	strArrCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+
+	var strArr []string
+
+	for i := 0; i < int(strArrCount); i++ {
+		strArrItem := readLine(reader)
+		strArr = append(strArr, strArrItem)
+	}
+
+	result := customSorting(strArr)
+
+	for i, resultItem := range result {
+		fmt.Fprintf(writer, "%s", resultItem)
+
+		if i != len(result)-1 {
+			fmt.Fprintf(writer, "\n")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
