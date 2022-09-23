0eb49c8bb8e382495a9bb4c338e6a6e01e6cfbc7
Nhat-Vu Nguyen <nhatvu148@gmail.com>
2022-09-23 15:18:25 +0900

    Create SelectionSort.py

---
+++ DataStructures/SelectionSort.py
@@ -0,0 +1,26 @@
+def selection_sort(my_list):
+    for i in range(len(my_list)-1):
+        min_index = i
+        for j in range(i+1, len(my_list)):
+            if my_list[j] < my_list[min_index]:
+                min_index = j
+        if i != min_index:
+            temp = my_list[i]
+            my_list[i] = my_list[min_index]
+            my_list[min_index] = temp
+    return my_list
+
+
+
+
+
+print(selection_sort([4,2,6,5,1,3]))
+
+ 
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [1, 2, 3, 4, 5, 6]
+ """
+
