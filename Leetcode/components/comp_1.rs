09cfd2460f13a102e8ce6c2390f418898597627d
Nhat-Vu Nguyen <nhatvu148@gmail.com>
2022-11-06 13:38:31 +0900

    Update comp_1.rs

--- Leetcode/components/comp_1.rs
+++ Leetcode/components/comp_1.rs
@@ -1,4 +1,59 @@
+use libc::c_char;
+use serde::{Deserialize, Serialize};
+use std::ffi::CStr;
 use std::{collections::HashMap, vec};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+extern "C" {
+    fn hello_world() -> *const c_char;
+}
+
+pub fn call_hello_world() -> &'static str {
+    unsafe {
+        CStr::from_ptr(hello_world())
+            .to_str()
+            .expect("String conversion failure")
+    }
+}
+
+pub fn pointer_example() {
+    let my_number = 1;
+    println!("my_number memory location {:p}", &my_number);
+    let my_number = 1;
+    let my_number_pointer: *const i32 = &my_number;
+    println!("my_number memory location {:p}", my_number_pointer);
+
+    let mut my_number = 1;
+    println!("my_number memory location {:p}", &mut my_number);
+    // or
+    let mut my_number = 1;
+    // let my_number_pointer: *const i32 = &mut my_number;
+    let my_number_pointer: *mut i32 = &mut my_number;
+    println!("my_number memory location {:p}", my_number_pointer);
+
+    let mut s = String::from("Hello world");
+    println!("{}", s);
+    let p = s.as_mut_ptr();
+    let c = unsafe { *p.add(6) } as char;
+    println!("{}", c);
+    unsafe { *p.add(6) = 'W' as u8 };
+    println!("{}", s);
+}
+
+pub fn serialized_example() {
+    let point = Point { x: 1, y: 2 };
+
+    let serialized = serde_json::to_string(&point).unwrap();
+    println!("serialized = {}", serialized);
+
+    let deserialized: Point = serde_json::from_str(&serialized).unwrap();
+    println!("deserialized = {:?}", deserialized);
+}
 
 pub fn create_linked_list_from_vec(l: Vec<i32>) -> Box<ListNode> {
     let mut linked_list = Box::new(ListNode::new(l[0]));
@@ -73,6 +128,20 @@ impl ListNode {
         node
     }
 }
+
+// let l1 = vec![1, 2, 4];
+// let l2 = vec![1, 3, 4];
+
+// let linked_list1 = create_linked_list_from_vec(l1);
+// println!("{:?}", &linked_list1);
+
+// let linked_list2 = create_linked_list_from_vec(l2);
+// println!("{:?}", &linked_list1);
+
+// println!(
+//     "{:?}",
+//     merge_two_lists(Some(linked_list1), Some(linked_list2))
+// );
 
 pub fn merge_two_lists(
     mut list1: Option<Box<ListNode>>,
