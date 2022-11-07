4f85767d8b928d44bb013734f6cea728846d18d9
Nhat-Vu Nguyen <nhatvu148@gmail.com>
2022-11-07 11:10:49 +0900

    Update main.rs

--- Leetcode/main.rs
+++ Leetcode/main.rs
@@ -1,36 +1,6 @@
 mod components;
-use std::num::ParseIntError;
-
 use components::comp_1::*;
 
-#[derive(Debug)]
-struct SummationError;
-
-fn to_int(s: &str) -> Result<i32, ParseIntError> {
-    // Option<i32> {
-    s.parse()
-    // s.parse().ok()
-}
-
-fn sum_str_vec(strs: Vec<String>) -> Result<String, SummationError> {
-    // Option<String> {
-    let mut accum = 0i32;
-    for s in strs {
-        accum += to_int(&s).map_err(|_| SummationError)?;
-        // accum += to_int(&s).ok_or(SummationError)?;
-        // accum += to_int(&s)?;
-    }
-
-    Ok(accum.to_string())
-    // Some(accum.to_string())
-}
-
 fn main() {
-    let v = vec![String::from("3"), String::from("4")];
-    let total = sum_str_vec(v);
-    println!("{:?}", total);
-
-    let v = vec![String::from("3"), String::from("abc")];
-    let total = sum_str_vec(v);
-    println!("{:?}", total);
+    result_example();
 }
