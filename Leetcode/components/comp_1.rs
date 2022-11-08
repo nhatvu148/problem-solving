0bc41134fcf1edc26baf9106817f84ca50c86c58
Nhat-Vu Nguyen <nhatvu148@gmail.com>
2022-11-08 17:24:34 +0900

    Update comp_1.rs

--- Leetcode/components/comp_1.rs
+++ Leetcode/components/comp_1.rs
@@ -4,6 +4,24 @@ use std::ffi::CStr;
 use std::num::ParseIntError;
 use std::{collections::HashMap, vec};
 
+pub fn make_good(s: String) -> String {
+    let diff_cases =
+        |c1: char, c2: char| c1.to_ascii_lowercase() == c2.to_ascii_lowercase() && c1 != c2;
+    s.chars()
+        .into_iter()
+        .fold(Vec::with_capacity(s.len()), |mut v: Vec<char>, c| {
+            match v.last() {
+                Some(prev_c) if diff_cases(c, *prev_c) => {
+                    v.pop();
+                }
+                _ => v.push(c),
+            }
+            v
+        })
+        .iter()
+        .collect()
+}
+
 pub fn maximum69_number(num: i32) -> i32 {
     let mut max = num;
     let num_str = num.to_string();
