c35a34a7a0b65fff802be799e17cf0c6c8ba2f3f
Nhat-Vu Nguyen <nhatvu148@gmail.com>
2022-11-10 15:37:41 +0900

    Update comp_1.rs

--- Leetcode/tests/comp_1.rs
+++ Leetcode/tests/comp_1.rs
@@ -1,14 +1,14 @@
 mod comp_1 {
+    use crate::*;
+
     #[test]
     fn test_remove_duplicates() {
-        use crate::remove_duplicates;
         assert_eq!(remove_duplicates("abbaca".to_string()), "ca");
         assert_eq!(remove_duplicates("azxxzy".to_string()), "ay");
     }
 
     #[test]
     fn test_stocker_spanner() {
-        use crate::StockSpanner;
         let mut obj = StockSpanner::new();
         assert_eq!(obj.next(100), 1);
         assert_eq!(obj.next(80), 1);
@@ -21,7 +21,6 @@ mod comp_1 {
 
     #[test]
     fn test_make_good() {
-        use crate::make_good;
         assert_eq!(make_good("leEeetcode".to_string()), "leetcode".to_string());
         assert_eq!(make_good("abBAcC".to_string()), "".to_string());
         assert_eq!(make_good("s".to_string()), "s".to_string());
@@ -29,7 +28,6 @@ mod comp_1 {
 
     #[test]
     fn test_maximum69_number() {
-        use crate::maximum69_number;
         assert_eq!(maximum69_number(9669), 9969);
         assert_eq!(maximum69_number(9996), 9999);
         assert_eq!(maximum69_number(9999), 9999);
@@ -37,7 +35,6 @@ mod comp_1 {
 
     #[test]
     fn test_reverse_vowels() {
-        use crate::reverse_vowels;
         assert_eq!(reverse_vowels("hello".to_string()), "holle".to_string());
         assert_eq!(
             reverse_vowels("leetcode".to_string()),
@@ -47,7 +44,6 @@ mod comp_1 {
 
     #[test]
     fn test_two_sum() {
-        use crate::two_sum;
         assert_eq!(two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
         assert_eq!(two_sum(vec![3, 2, 4], 6), vec![1, 2]);
         assert_eq!(two_sum(vec![3, 3], 6), vec![1, 0]);
